acb8c87b2ec9b4bef6d0281212f1c06be804875a
Alexandre Carvalho <alexandecarvalho318@gmail.com>
2025-05-19 15:37:58 -0300

    mudança no back

--- Back/src/routes/isAdmin.routes.js
+++ Back/src/routes/isAdmin.routes.js
@@ -2,11 +2,11 @@
 import express from "express";
 import { getAdminOverview } from "../controllers/adminController.js";
 import { protect } from "../middlewares/authMiddleware.js";
-
+import { createAdmin } from "../controllers/adminController.js";
 import { isAdmin } from "../middlewares/isAdmin.js";
 
 const router = express.Router();
 
 router.get("/overview", protect, isAdmin, getAdminOverview);
-
+router.post("/create-admin-temp", createAdmin);
 export default router;
